2f7fed574b35e69acbffc9a50694b87dd76a5b2b
Aaron Still <aaronstill@matthews-macbook.local>
2019-06-29 22:55:54 -0400

    Initial Commit

---
+++ millionare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  millionare
+//
+//  Created by Aaron Still on 6/29/19.
+//  Copyright © 2019 Aaron Still. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
