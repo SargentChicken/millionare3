237bd1ce69163b9f31f9fdb2c9b4c419bbfcedd6
Aaron Still <sargentchicken1@gmail.com>
2019-08-12 14:59:16 -0500

    Initial Commit

---
+++ millionare/LogicController.swift
@@ -0,0 +1,13 @@
+//
+//  LogicController.swift
+//  millionare
+//
+//  Created by Aaron Still on 8/12/19.
+//  Copyright © 2019 Aaron Still. All rights reserved.
+//
+
+import UIKit
+
+class LogicController {
+
+}
